2031a827c319e8c9432ffe15c18a800f8759c6cc
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-23 00:34:11 +0600

    add sing in

--- src/Pages/Routes/OpenRoutes.js
+++ src/Pages/Routes/OpenRoutes.js
@@ -1,6 +1,8 @@
 import Home from "../Home/Home";
+import SingIn from "../User/SingIn";
 
 export const openRoutes = [
   { path: "/", Component: Home },
   { path: "home", Component: Home },
+  { path: "singIn", Component: SingIn },
 ];
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "../../Pages/CssFile/AllCss.css";
 const Header = () => {
   const [hambarger, setHambarger] = useState(false);
