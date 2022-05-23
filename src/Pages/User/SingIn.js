455e77cbe25d975d0ff897a593c08493f518d4c6
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-23 12:00:45 +0600

    add color

--- src/Pages/User/SingIn.js
+++ src/Pages/User/SingIn.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import "../../Pages/CssFile/AllCss.css";
 import {
   FaFacebook,
   FaGithub,
   FaGoogle,
   FaKey,
+  FaLock,
   FaUserCircle,
 } from "react-icons/fa";
 const SingIn = () => {
@@ -18,31 +20,41 @@ const SingIn = () => {
     <div className="singIn-container">
       <div className="forms-container">
         <div className="logSingInForm">
+          <div className="new-here">
+            <h2>New To Here? </h2>
+            <p>If you are new to here?Sing in first</p>
+            <input className="btn-2" type="submit" value="SING UP" />
+          </div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <h2>Sing in</h2>
+            <h2 className="singInTitle">Sing in</h2>
             <div className="inputField">
-              <FaUserCircle />
-              <input type="text" {...register("Name", { required: true })} />
+              <FaUserCircle className="input-icon" />
+              <input
+                type="text"
+                placeholder="Email"
+                {...register("Name", { required: true })}
+              />
             </div>
             {errors.firstName?.type === "required" &&
               "First   name is required"}
 
             <div className="inputField">
-              <FaKey />
+              <FaLock className="input-icon" />
               <input
                 type="password"
+                placeholder="Password"
                 {...register("Password", { pattern: /^[A-Za-z]+$/i })}
               />
             </div>
 
             {errors.lastName && "Last name is required"}
 
-            <input type="submit" />
+            <input className="sing-up-btn" value="SING IN" type="submit" />
             <div class="divider">OR</div>
-            <div className="socialMediaIcon">
-              <FaGoogle />
-              <FaFacebook />
-              <FaGithub />
+            <div className="social_container">
+              <FaGoogle className="socialMediaIcon" />
+              <FaFacebook className="socialMediaIcon" />
+              <FaGithub className="socialMediaIcon" />
             </div>
           </form>
         </div>
