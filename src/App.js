15cb980138d12d34a88cc9b7b7ca0803250a894b
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-24 16:03:31 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./styles.css";
 import Header from "./Pages/Shared/Header";
 import { openRoutes } from "./Pages/Routes/OpenRoutes";
+import Footer from "./Pages/Shared/Footer";
 function App() {
   return (
     <div className="styles">
@@ -11,6 +12,7 @@ function App() {
           <Route key={index} path={path} element={<Component />}></Route>
         ))}
       </Routes>
+      <Footer />
     </div>
   );
 }
