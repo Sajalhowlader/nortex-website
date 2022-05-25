65f34be3bf6f318d02898b25f7f6b8bd00bf6e85
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-25 18:53:59 +0600

    add book now

--- src/Pages/Routes/Protected.js
+++ src/Pages/Routes/Protected.js
@@ -2,10 +2,14 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import auth from "../../firebaseCredential";
+import PreLoader from "../Shared/PreLoader";
 
-const Protected = ({ children }) => {
-  const [user] = useAuthState(auth);
+const Protected = () => {
+  const [user, isLoading] = useAuthState(auth);
   const location = useLocation();
+  if (isLoading) {
+    return <PreLoader />;
+  }
   if (!user) {
     return <Navigate to="/singIn" state={{ from: location }} replace />;
   }
--- src/UnderPrivete/Purchase.js
+++ src/UnderPrivete/Purchase.js
@@ -10,8 +10,28 @@ const Purchase = () => {
       .then((res) => res.json())
       .then((data) => setItems(data));
   }, [purchaseId]);
-
-  return <div>hello{items.name}</div>;
+  const { name, img, minOrder, dis, price, available } = items;
+  return (
+    <section>
+      <div className="title">
+        <h1>ORDER NOW</h1>
+      </div>
+      <div className="mx-auto container order-container">
+        <div className="order-img">
+          <img src={img} alt="" />
+        </div>
+        <div className="order-info">
+          <h2>Name:{name}</h2>
+          <div className="min-max">
+            <h2>Min Order:{minOrder}</h2>
+            <h2>Available:{available}</h2>
+          </div>
+          <h1>Price: ${price}</h1>
+          <p>About:{dis.slice(0, 150) + "..."}</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Purchase;
