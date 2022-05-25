acc0f5c51d3e59e06cb690cbe46f28015d774a39
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-25 19:27:02 +0600

    add boook now style

--- src/UnderPrivete/Purchase.js
+++ src/UnderPrivete/Purchase.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 const Purchase = () => {
   const { purchaseId } = useParams();
+  const [isReload, setIsReload] = useState(false);
   const [items, setItems] = useState({});
   useEffect(() => {
     const url = `http://localhost:5000/tools/${purchaseId}`;
@@ -10,7 +11,9 @@ const Purchase = () => {
       .then((res) => res.json())
       .then((data) => setItems(data));
   }, [purchaseId]);
+  console.log(items);
   const { name, img, minOrder, dis, price, available } = items;
+
   return (
     <section>
       <div className="title">
@@ -21,13 +24,13 @@ const Purchase = () => {
           <img src={img} alt="" />
         </div>
         <div className="order-info">
-          <h2>Name:{name}</h2>
+          <h2 className="order-name">Name:{name}</h2>
           <div className="min-max">
             <h2>Min Order:{minOrder}</h2>
             <h2>Available:{available}</h2>
           </div>
-          <h1>Price: ${price}</h1>
-          <p>About:{dis.slice(0, 150) + "..."}</p>
+          <h1 className="order-price">Price: ${price}</h1>
+          <p>About:{dis}</p>
         </div>
       </div>
     </section>
