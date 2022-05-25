c77864d47fb70d3fe0c41e405dfd1f38ff5b2fd7
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-25 20:11:29 +0600

    add avater

--- src/UnderPrivete/Purchase.js
+++ src/UnderPrivete/Purchase.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
+import auth from "../firebaseCredential";
 
 const Purchase = () => {
+  const [user] = useAuthState(auth);
   const { purchaseId } = useParams();
   const [isReload, setIsReload] = useState(false);
   const [items, setItems] = useState({});
@@ -15,9 +18,22 @@ const Purchase = () => {
   const { name, img, minOrder, dis, price, available } = items;
 
   return (
-    <section>
+    <section className="order-section">
       <div className="title">
         <h1>ORDER NOW</h1>
+      </div>
+      <div className="user-container">
+        {
+          <div class="user-info">
+            <div class=" avatar online">
+              <img className="w-24 rounded-full" src={user.photoURL} alt="" />{" "}
+            </div>
+            <div>
+              <p className="">{user.displayName}</p>
+              <p className="">{user.email}</p>
+            </div>
+          </div>
+        }
       </div>
       <div className="mx-auto container order-container">
         <div className="order-img">
