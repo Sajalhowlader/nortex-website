d876272f2241d51314650e345ed740004613acf6
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-25 10:13:16 +0600

    add done

--- src/Pages/Home/Contacts.js
+++ src/Pages/Home/Contacts.js
@@ -3,16 +3,23 @@ import csImg from "../../images/svg/5124556.jpg";
 const Contacts = () => {
   return (
     <section>
+      <div className="title">
+        <h1>CONTACT US</h1>
+      </div>
       <div className="container mx-auto c-container">
         <div className="c-img">
           <img src={csImg} alt="" />
         </div>
-        <div className="cs-from">
+        <div className="cs-from mx-auto">
           <input type="text" className="c-info" placeholder="Name" />
           <br />
           <input type="text" className="c-info" placeholder="Email" />
           <br />
-          <textarea className="c-info" placeholder="Your Message"></textarea>
+          <textarea
+            className="c-info h-36"
+            placeholder="Your Message"
+          ></textarea>
+          <button class="care-btn care-2">submit</button>
         </div>
       </div>
     </section>
