8f6625d6174c6e456a884fd39fd916e85e5f46ec
Sajalhowlader <sajolhowlader08@gmail.com>
2022-06-22 14:29:40 +0600

    add new summery

--- src/Pages/Home/ShowTools.js
+++ src/Pages/Home/ShowTools.js
@@ -1,7 +1,7 @@
 import { FaStar } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 const ShowTools = ({ tool }) => {
-  const { _id, name, img, dis, price, minOrder, available,star } = tool;
+  const { _id, name, img, dis, price, minOrder, available, star } = tool;
   const navigate = useNavigate();
 
   const handlePurchase = (id) => {
@@ -16,9 +16,11 @@ const ShowTools = ({ tool }) => {
         <div className="tools-containers">
           <div className="card-img">
             <img src={img} alt="" />
-            {[...Array(parseInt(star))].map((start, index) => {
-                  return <FaStar className="mx-1" key={index} />
-                })}
+            <p className="stars mt-[-130px] text-[#e90e3d] ">
+              {[...Array(parseInt(star))].map((start, index) => {
+                return <FaStar className="my-2" key={index} />;
+              })}
+            </p>
             <h2>{name}</h2>
           </div>
           <div className="card-info">
@@ -27,7 +29,6 @@ const ShowTools = ({ tool }) => {
               <p>Min Order: {minOrder}</p>
               <p>Available: {available}</p>
             </div>
-
             <p className="dis">Description:{dis.slice(0, 70) + "..."}</p>
             <div className="mx-auto order-btn">
               <button onClick={() => handlePurchase(_id)} className="care-btn">
